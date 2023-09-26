f7dd9b51a160ffa4211d2b44a0cda0a9410ba6fe
zg <864610252@qq.com>
2023-09-26 20:29:16 +0800

    'add'

--- hello/hello.go
+++ hello/hello.go
@@ -1,11 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 var i, j int = 1, 2
+var s = "foo bar baz"
 
 func main() {
 	fmt.Println("Hello,test!")
 	var c, python, java = true, false, "no!"
 	fmt.Println(i, j, c, python, java)
+	b := make(map[string]int)
+
+	a := strings.Fields(s)
+	fmt.Println(a)
+	fmt.Printf("Fields are: %q", strings.Fields("foo bar baz"))
 }
